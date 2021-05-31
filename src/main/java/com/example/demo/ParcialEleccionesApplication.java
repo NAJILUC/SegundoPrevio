d6a7c0cbab8f0b8d8241c3e53281413c840e96f9
Najiluc <33139457+NAJILUC@users.noreply.github.com>
2021-05-31 08:33:56 -0500

    Add files via upload

---
+++ src/main/java/com/example/demo/ParcialEleccionesApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParcialEleccionesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParcialEleccionesApplication.class, args);
+	}
+
+}

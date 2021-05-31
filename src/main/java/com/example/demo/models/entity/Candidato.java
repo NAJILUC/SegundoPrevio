1567ff80672ce0a6c4e86dd22f2ff4da8dd0e71a
Najiluc <33139457+NAJILUC@users.noreply.github.com>
2021-05-31 09:38:03 -0500

    Commit 3

--- src/main/java/com/example/demo/models/entity/Candidato.java
+++ src/main/java/com/example/demo/models/entity/Candidato.java
@@ -10,7 +10,7 @@ import javax.persistence.Table;
 
 @Entity
 
-@Table(name="clientes") 
+@Table(name="candidatos") 
 public class Candidato implements Serializable{
 
 	private static final long serialVersionUID = 1L;
